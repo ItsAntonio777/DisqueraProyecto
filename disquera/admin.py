fed6b8328df147aad0fe8f4bad0079611e83a464
ItsAntonio777 <luisvideostj@gmail.com>
2026-05-17 22:52:13 -0700

    implementar vistas home y detail con renderizado dinámico de publicaciones, imágenes, ficha técnica y comentarios

--- disquera/admin.py
+++ disquera/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Post, Comment, Specification
 
-# Register your models here.
+
+class SpecificationInline(admin.TabularInline):
+   model = Specification
+   extra = 2
+
+
+class PostAdmin(admin.ModelAdmin):
+   list_display = ('title', 'status', 'created_at')
+
+   inlines = [SpecificationInline]
+
+   prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
