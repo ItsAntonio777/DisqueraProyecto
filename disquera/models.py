4cb936e6a995be8869248ba8da8ce2acc1991fea
kevinmartinez43 <km9767721@gmail.com>
2026-05-17 21:10:08 -0700

    lo que realice aqui fu ijacomodar el models añadir categorías, posts, especificaciones y comentarios al proyecto

--- disquera/models.py
+++ disquera/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+   title = models.CharField(max_length=255)
+   slug = models.SlugField(max_length=255, unique=True, null=True, blank=True)
+   color_hex = models.CharField(max_length=7, default="#698CDD")
+
+   def __str__(self):
+       return self.title
+
+
+class Post(models.Model):
+
+   ACTIVE = 'active'
+   DRAFT = 'draft'
+
+   CHOICES_STATUS = (
+       (ACTIVE, 'Active'),
+       (DRAFT, 'Draft'),
+   )
+
+   category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
+
+   title = models.CharField(max_length=255)
+   slug = models.SlugField(max_length=255)
+
+   intro = models.TextField()
+   body = models.TextField()
+
+   created_at = models.DateTimeField(auto_now_add=True)
+
+   status = models.CharField(
+       max_length=10,
+       choices=CHOICES_STATUS,
+       default=ACTIVE
+   )
+
+   image = models.ImageField(upload_to='upload/', blank=True, null=True)
+
+   def __str__(self):
+       return self.title
+
+   def get_absolute_url(self):
+       return f'/album/{self.id}/'
+
+
+class Specification(models.Model):
+
+   post = models.ForeignKey(Post, related_name='specs', on_delete=models.CASCADE)
+
+   label = models.CharField(max_length=50)
+   value = models.CharField(max_length=255)
+
+   def __str__(self):
+       return f"{self.label}: {self.value}"
+
+
+class Comment(models.Model):
+
+   post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+
+   name = models.CharField(max_length=255)
+
+   email = models.EmailField()
+
+   body = models.TextField()
+
+   created_at = models.DateTimeField(auto_now_add=True)
+
+   def __str__(self):
+       return f"Comentario de {self.name} en {self.post.title}"
