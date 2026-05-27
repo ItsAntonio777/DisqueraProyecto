3463a1b4df93f8a19971024890e5e90c5d9b0568
itsAntonio777 <luisvideostj@gmail.com>
2026-05-27 01:29:58 -0700

    Implementación de formularios personalizados y pantalla de compra exitosa

--- disquera/forms.py
+++ disquera/forms.py
@@ -1,9 +1,23 @@
 from django import forms
-from .models import Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import Comment, Order
 
 
 class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
 
-   class Meta:
-       model = Comment
-       fields = ['name', 'email', 'body']
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class CheckoutForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['full_name', 'address', 'city', 'postal_code']
