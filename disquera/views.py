faa27c1597f5c29a61d03bb56f7595f6eb0e5c38
itsBarajas777 <axelbarajas000@gmail.com>
2026-06-01 16:04:30 -0700

    arreglos de el dia 01/06/26

--- disquera/views.py
+++ disquera/views.py
@@ -43,12 +43,12 @@ def home(request):
         'categories': categories
     })
 
-
 # CATEGORY
 def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
 
-    posts = Post.objects.filter(category=category, status=Post.ACTIVE)
+    # REEMPLAZADO: 'category=category' por 'categories=category'
+    posts = Post.objects.filter(categories=category, status=Post.ACTIVE)
 
     return render(request, 'disquera/category_detail.html', {
         'category': category,
